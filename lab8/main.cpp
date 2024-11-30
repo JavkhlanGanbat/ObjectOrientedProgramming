82fffe472a7ce265b73c1ce1d7e07407dd1cf7b3
Javkhlan Ganbat <javkhlan.ganbat2003@gmail.com>
2024-11-30 19:41:46 +0800

    static members, deletion

---
+++ lab8/main.cpp
@@ -0,0 +1,146 @@
+#include "shapes.hpp"
+
+vector<TwoDShape*> shapes;
+
+void displayShapes() {
+    for (int i = 0; i < shapes.size(); ++i) {
+        cout << i + 1 << ". " << shapes[i]->getName() << endl
+             << "Position: (" << shapes[i]->getX() << ", " << shapes[i]->getY() << ")" << endl
+             << "Area: " << shapes[i]->getArea() << endl
+             << "Perimeter: " << shapes[i]->getPerimeter() << endl << endl;
+    }
+    cout << Shape::getObjCount() << " Objects exist" <<  endl << endl;
+}
+
+void createCircle() {
+    double x, y, radius;
+    cout << "Enter X, Y, radius of the circle: ";
+    cin >> x >> y >> radius;
+
+    Circle* cir = new Circle();
+    cir->setX(x);
+    cir->setY(y);
+    cir->setRadius(radius);
+
+    shapes.push_back(cir);
+}
+
+void createSquare() {
+    double x, y, sideLength;
+    cout << "Enter X, Y, side length of the square: ";
+    cin >> x >> y >> sideLength;
+
+    Square* sq = new Square();
+    sq->setX(x);
+    sq->setY(y);
+    sq->setSideLength(sideLength);
+
+    shapes.push_back(sq);
+}
+
+void createTriangle() {
+    double x, y, sideLength;
+    cout << "Enter X, Y, side length of the triangle: ";
+    cin >> x >> y >> sideLength;
+
+    Triangle* tri = new Triangle();
+    tri->setX(x);
+    tri->setY(y);
+    tri->setSideLength(sideLength);
+
+    shapes.push_back(tri);
+}
+void sortShapesByArea() {
+    for (int i = 0; i < shapes.size() - 1; ++i) {
+        for (int j = 0; j < shapes.size() - i - 1; ++j) {
+            if (shapes[j]->getArea() > shapes[j + 1]->getArea()) {
+                TwoDShape* temp = shapes[j];
+                shapes[j] = shapes[j + 1];
+                shapes[j + 1] = temp;
+            }
+        }
+    }
+    cout << "Done.\n";
+}
+
+void sortShapesByPerimeter() {
+    for (int i = 0; i < shapes.size() - 1; ++i) {
+        for (int j = 0; j < shapes.size() - i - 1; ++j) {
+            if (shapes[j]->getPerimeter() > shapes[j + 1]->getPerimeter()) {
+                TwoDShape* temp = shapes[j];
+                shapes[j] = shapes[j + 1];
+                shapes[j + 1] = temp;
+            }
+        }
+    }
+    cout << "Done.\n";
+}
+void deleteShapeObject() {
+    int del;
+    cout << "Index of object to delete: ";
+    cin >> del;
+
+    if (del < 1 || del > shapes.size()) {
+        cout << "Out of bounds.\n";
+        return;
+    }
+
+    del -= 1;
+
+    delete shapes[del];
+    shapes[del] = nullptr;
+    shapes.erase(shapes.begin() + del);
+    cout << "Done.\n";
+}
+
+int main() {
+    int choice;
+
+    do {
+        cout << "1. Circle\n";
+        cout << "2. Square\n";
+        cout << "3. Triangle\n";
+        cout << "4. Print objects\n";
+        cout << "5. Sort by area\n";
+        cout << "6. Sort by perimeter\n";
+        cout << "7. Delete object\n";
+        cout << "0. Exit\n";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                createCircle();
+                break;
+            case 2:
+                createSquare();
+                break;
+            case 3:
+                createTriangle();
+                break;
+            case 4:
+                displayShapes();
+                break;
+            case 5:
+                sortShapesByArea();
+                break;
+            case 6:
+                sortShapesByPerimeter();
+                break;
+            case 7:
+                deleteShapeObject();
+                break;
+            case 0:
+                cout << "Bye.\n";
+                break;
+            default:
+                cout << "No.\n";
+                break;
+        }
+    } while (choice != 0);
+
+    for (TwoDShape* shape : shapes) {
+        delete shape;
+    }
+
+    return 0;
+}
---
+++ lab8/shapes.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <math.h> 
+#include <cstring> 
+
+using namespace std; 
+
+class Shape {
+    protected:
+        char* name;
+    public:
+        static int ObjCount;
+        Shape(const char* name = "");
+        Shape(const Shape& other);
+        Shape& operator=(const Shape& other);
+        virtual ~Shape();
+        void setName(const char* name);
+        const char* getName();
+        static int getObjCount();
+};
+
+class TwoDShape : virtual public Shape{ 
+    protected: 
+        double x; 
+        double y; 
+        double sideLength; 
+    public: 
+        double getSideLength(); 
+        double getX(); 
+        double getY(); 
+        void setSideLength(double sideLength); 
+        void setX(double x); 
+        void setY(double y); 
+        TwoDShape(double x = 0, double y = 0, double sideLength = 0, const char* name = "o"); 
+        virtual double getArea() = 0; 
+        virtual double getPerimeter() = 0;
+}; 
+ 
+class Circle : public TwoDShape{ 
+    public: 
+        Circle(double x = 0, double y = 0, double radius = 0); 
+        double getRadius(); 
+        void setRadius(double radius); 
+        void setCenterX(double x); 
+        void setCenterY(double y); 
+        double getCenterX(); 
+        double getCenterY(); 
+        double getCircumference(); 
+        double getArea() override; 
+        double getPerimeter() override; 
+        const char* getShapeName(); 
+}; 
+ 
+class Square : public TwoDShape { 
+    public: 
+        Square(double x = 0, double y = 0, double sideLength = 0); 
+        double getSideLength(); 
+        void setSideLength(double sideLength); 
+        double getArea() override; 
+        double getPerimeter() override; 
+        const char* getShapeName(); 
+    
+        double getTopLeftX(); 
+        double getTopLeftY(); 
+        double getTopRightX(); 
+        double getTopRightY(); 
+        double getBottomLeftX(); 
+        double getBottomLeftY(); 
+        double getBottomRightX(); 
+        double getBottomRightY();
+}; 
+ 
+class Triangle : public TwoDShape { 
+    public: 
+        Triangle(double x = 0, double y = 0, double sideLength = 0); 
+        double getSideLength(); 
+        void setSideLength(double sideLength); 
+        double getArea() override; 
+        double getPerimeter() override; 
+        const char* getShapeName(); 
+    
+        double getTopX(); 
+        double getTopY(); 
+        double getBottomLeftX(); 
+        double getBottomLeftY(); 
+        double getBottomRightX(); 
+        double getBottomRightY(); 
+    
+    private: 
+        double calculateHeight(); 
+};
