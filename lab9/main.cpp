c79a07f3ee9894d94f4242f09e3c85ed41b249fc
Javkhlan Ganbat <javkhlan.ganbat2003@gmail.com>
2024-12-05 12:53:05 +0800

    .

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,5 +1,4 @@
 #include "headers/Employee.hpp"
-
 #include <iostream>
 using namespace std;
 
@@ -8,18 +7,25 @@ int main() {
     Division division("Software development");
     JobDescription jobDesc1("Software engineer");
     JobDescription jobDesc2("Project manager");
-    
     vector<JobDescription> jobDescriptions = {jobDesc1, jobDesc2};
-    
     Spouse spouse("Jane Smith", "123-45-6789", 30, Date(1, 1, 2010));
-    
     Child child1("Alice", "987-65-4321", 5, "Lego");
     Child child2("Kyle", "4322-4322-4322", 4, "Lego");
-    
     vector<Child> children = {child1, child2};
 
     Employee employee("John Smith", "123-45-6789", 35, "001", "Senior",
                       startDate, division, jobDescriptions, spouse, children);
+
+    cout << "Initial Employee Details:\n";
+    employee.display();
+
+    employee.setCompanyID("002");
+    employee.setTitle("Lead dev");
+    employee.setStartDate(Date(2, 2, 2022));
+    employee.setSpouse(Spouse("Emily Smith", "456-78-9123", 28, Date(5, 5, 2015)));
+
+    employee.addJobDescription(JobDescription("Technical lead"));
+    employee.addChild(Child("Emma", "555-55-5555", 3, "Barbie"));
 
     employee.display();
 
@@ -28,6 +34,6 @@ int main() {
     employee.deleteChild(1);
 
     employee.display();
+
     return 0;
 }
-
